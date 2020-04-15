9b12119b2d673972cb6a348a83578de8e4067992
Jovane Marques <jovanemarques@gmail.com>
2020-04-15 17:04:51 -0400

    delete recipe is working

--- src/containers/Recipe/recipe.list.js
+++ src/containers/Recipe/recipe.list.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter, useHistory } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Alert from 'react-bootstrap/Alert';
 import Table from 'react-bootstrap/Table';
 import {Link} from 'react-router-dom'
 import axios from 'axios';
@@ -11,7 +13,9 @@ function RecipeList (props) {
   const { screen, setScreen } = props;
   // return a stateful value and funcion to update it
   const [data, setData] = useState();
-
+  const [showAlert, setShowAlert] = useState();
+  const [showModal, setShowModal] = useState();
+  const [showModalResult, setShowModalResult] = useState();
   var [recipes, setRecipes] = useState([
     // {
     //     name: "Recipe 1",
@@ -46,8 +50,65 @@ function RecipeList (props) {
         history.push(`/recipes/edit/${id}`);
     }
 
+    const clickDelete = (id) => {
+        const deleteFn = async (id) => {
+            console.log('delete:'+id);
+            const res = await axios.delete(`${props.apiUrl}/recipe/${id}`);
+            if (res && res.data && res.data.error){
+                console.log('Error: ' + res.data.error);
+            } else if (res && res.data){
+                // console.log('res->' + JSON.stringify(res.data));
+                setRecipes(recipes.filter(i => i._id != id));
+                setShowAlert({tltle:'Success', message:'Recipe deleted!'});
+            } 
+        };
+        setShowModal({title:'Attention!', message:'Are you sure that you want to delete?', cbFunctionYes: () => deleteFn(id)});
+    }
+
+    const drawAlert = () => {
+        if (showAlert){
+            return (
+                <Alert variant="danger" onClose={() => setShowAlert()} dismissible>
+                    <Alert.Heading>{showAlert.title}</Alert.Heading>
+                    <p>
+                    {showAlert.message}
+                    </p>
+                </Alert>
+            );
+        }
+    }
+
+    const drawModal = () => {
+        if (showModal){
+            return (
+                <Modal show={showModal || false} aria-labelledby="contained-modal-title-vcenter" centered>
+                    <Modal.Header>
+                        <Modal.Title>{showModal.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>{showModal.message}</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => {setShowModal()}}>
+                            No
+                        </Button>
+                        <Button variant="primary" 
+                            onClick={() => {
+                                setShowModal();
+                                setShowModalResult(true);
+                                showModal.cbFunctionYes();
+                            }}
+                        >
+                            Yes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            );
+        }
+    }
+
   return (
     <div className="App">
+        {drawAlert()}
+        {drawModal()}
       {recipes && recipes.length > 0
         ? <div>
             <h1>Recipes: </h1>
@@ -66,13 +127,13 @@ function RecipeList (props) {
                   </thead>
                   <tbody>
                     {recipes.map((recipe, idx) => (
-                        <tr>
+                        <tr key={idx}>
                         <td>{idx}</td>
                         <td>{recipe.name}</td>
                         <td>
                             <Button type="button" variant="primary">Use</Button> &nbsp;
                             <Button type="button" variant="primary" onClick={() => clickEdit(recipe._id)}>Edit</Button> &nbsp;
-                            <Button type="button" variant="primary">Delete</Button>
+                            <Button type="button" variant="primary" onClick={() => clickDelete(recipe._id)}>Delete</Button>
                         </td>
                     </tr>
                     ))}
