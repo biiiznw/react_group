255fe7047237f6e7dfc2ebabbba2e261e0e11e59
Jovane Marques <jovanemarques@gmail.com>
2020-04-15 16:55:40 -0400

    delete item working

--- src/containers/Item/item.list.js
+++ src/containers/Item/item.list.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import Modal from 'react-bootstrap/Modal';
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 import {Link, useHistory} from 'react-router-dom';
@@ -16,6 +16,9 @@ const ListItem = props => {
     const [items, setItems] = useState([
         // {name:'Item 1', base_unit:'kg', qty:100}
     ]);
+    const [showAlert, setShowAlert] = useState();
+    const [showModal, setShowModal] = useState();
+    const [showModalResult, setShowModalResult] = useState();
     const [showLoading, setShowLoading] = useState(true);
     // const apiUrl = "http://localhost:3001/api/items";
 
@@ -36,8 +39,65 @@ const ListItem = props => {
         history.push(`/items/edit/${id}`);
     }
 
+    const clickDelete = (id) => {
+        const deleteFn = async (id) => {
+            console.log('delete:'+id);
+            const res = await axios.delete(`${props.apiUrl}/item/${id}`);
+            if (res && res.data && res.data.error){
+                console.log('Error: ' + res.data.error);
+            } else if (res && res.data){
+                // console.log('res->' + JSON.stringify(res.data));
+                setItems(items.filter(i => i._id != id));
+                setShowAlert({tltle:'Success', message:'Item deleted!'});
+            } 
+        };
+        setShowModal({title:'Attention!', message:'Are you sure that you want to delete?', cbFunctionYes: () => deleteFn(id)});
+    }
+
+    const drawAlert = () => {
+        if (showAlert){
+            return (
+                <Alert variant="danger" onClose={() => setShowAlert()} dismissible>
+                    <Alert.Heading>{showAlert.title}</Alert.Heading>
+                    <p>
+                    {showAlert.message}
+                    </p>
+                </Alert>
+            );
+        }
+    }
+
+    const drawModal = () => {
+        if (showModal){
+            return (
+                <Modal show={showModal || false} aria-labelledby="contained-modal-title-vcenter" centered>
+                    <Modal.Header>
+                        <Modal.Title>{showModal.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>{showModal.message}</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => {setShowModal()}}>
+                            No
+                        </Button>
+                        <Button variant="primary" 
+                            onClick={() => {
+                                setShowModal();
+                                setShowModalResult(true);
+                                showModal.cbFunctionYes();
+                            }}
+                        >
+                            Yes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            );
+        }
+    }
+
     return (
         <div>
+            {drawAlert()}
+            {drawModal()}
         {items && items.length > 0
           ? 
             <div>
@@ -63,14 +123,14 @@ const ListItem = props => {
                   </thead>
                   <tbody>
                     {items.map((item, idx) => (
-                        <tr>
+                        <tr key={idx}>
                             <td>{idx}</td>
                             <td>{item.name}</td>
                             <td>{item.quantity}</td>
                             <td>{item.baseUnit}</td>
                             <td>
                                 <Button type="button" variant="primary" onClick={() => clickEdit(item._id)}>Edit</Button> &nbsp;
-                                <Button type="button" variant="primary">Delete</Button>
+                                <Button type="button" variant="primary" onClick={() => clickDelete(item._id)}>Delete</Button>
                             </td>
                         </tr>
                     ))}
